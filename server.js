190a59f2dd1573e887bdb068dda73b6d690b5872
furimako <furimako@gmail.com>
2018-01-20 13:56:44 +0900

    update

--- server.js
+++ server.js
@@ -1,16 +1,40 @@
 const http = require('http')
 const fs = require('fs')
+const url = require('url')
 
-var server = http.createServer(
-    (request, response) => {
-        fs.readFile('./html/index.html', 'UTF-8', 
-            (error, data) => {
-                response.writeHead(200, {'Content-Type': 'text/html'})
-                response.write(data)
-                response.end()
-            }
-        )
+var server = http.createServer(requestListenerFunction)
+server.listen(8128)
+console.log('Server Start')
+
+
+function requestListenerFunction(request, response) {
+    var url_parts = url.parse(request.url)
+    var target
+    var contentType
+    switch (url_parts.pathname) {
+        case "/":
+            target = './static/index.html'
+            contentType = 'text/html'
+            break
+        case "/markdown.css":
+            target = './static/markdown.css'
+            contentType = 'text/css'
+            break
+        default:
+            response.writeHead(200, {
+                'Content-Type': 'plain'
+            })
+            response.end('no page...')
+            return
     }
-)
-server.listen(3000)
-console.log('Server Start!')
+
+    fs.readFile(target, 'UTF-8',
+        (error, data) => {
+            response.writeHead(200, {
+                'Content-Type': contentType
+            })
+            response.write(data)
+            response.end()
+        }
+    )
+}
