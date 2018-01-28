dd866adf126d77c88b18dcc67782a3d01f023c24
furimako <furimako@gmail.com>
2018-01-28 21:25:19 +0900

    add description tag

--- server.js
+++ server.js
@@ -5,16 +5,58 @@ const Pages = require('./scripts/pages')
 
 // Create pages
 let pages = new Pages()
-pages.addPage('Home', '/', './static/index.html')
-pages.addPage('No Found', false, './static/no-found.html')
-pages.addPage('「幸せ」を目指してはいけない', '/ideology/your-goal')
-pages.addPage('あなたにとっての「幸せ」は？', '/ideology/happiness')
-pages.addPage('今を生きるためのちょっとしたヒント', '/ideology/tips-for-your-life')
-pages.addPage('「自由」に生きてはいけない', '/ideology/freedom')
-pages.addPage('「大人」について', '/ideology/grown-up')
-pages.addPage('理想の「教育」', '/ideology/education')
-pages.addPage('「哲学」は無意味である (仮)', '/ideology/philosophy')
-pages.addPage('「一番」を目指してはいけない', '/ideology/ideal-way-to-live-life')
+pages.addPage(
+    'Home',
+    'キミの知らない世界を見せよう。',
+    '/',
+    './static/index.html'
+)
+pages.addPage(
+    'No Found',
+    '',
+    false,
+    './static/no-found.html'
+)
+pages.addPage(
+    '「幸せ」を目指してはいけない',
+    '多くの人は「幸せ」を求めて日々生きているのではないだろうか。',
+    '/ideology/your-goal'
+)
+pages.addPage(
+    'あなたにとっての「幸せ」は？',
+    '「幸せ」の定義は何でしょうか？ あなたが気づいていない、「幸せ」の一つの姿について。',
+    '/ideology/happiness'
+)
+pages.addPage(
+    '今を生きるためのちょっとしたヒント',
+    '私のメモ帳の中身。気をつけないと忘れてしまうこと。でもとっても大切なこと。',
+    '/ideology/tips-for-your-life'
+)
+pages.addPage(
+    '「自由」に生きてはいけない',
+    'きっとキミは思うだろう。そんなはずはない！ と。でもいつかキミはこう思うだろう。確かにそうかもしれない、と。',
+    '/ideology/freedom'
+)
+pages.addPage(
+    '「大人」について',
+    'あなたは本当に「大人」ですか？',
+    '/ideology/grown-up'
+)
+pages.addPage(
+    '理想の「教育」',
+    '「歴史のテスト」を憎むすべての人に、このページを捧げます。',
+    '/ideology/education'
+)
+pages.addPage(
+    '「哲学」は無意味である (仮)',
+    '「この世界はどうして存在するのだろう？」Fully Hatter が答えましょう。',
+    '/ideology/philosophy'
+)
+pages.addPage(
+    '「一番」を目指してはいけない',
+    '「一番」を目指さずに、いったい何を目指すべきだというのか！ そんなあなたのためのページです。',
+    '/ideology/ideal-way-to-live-life'
+)
 
 // Start server
 let server = http.createServer(requestListenerFunction)
