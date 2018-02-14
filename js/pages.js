ccd371c407840728b51ae6d2eeca5783fa0cccb7
furimako <furimako@gmail.com>
2018-02-14 21:28:06 +0900

    add views.json

--- js/pages.js
+++ js/pages.js
@@ -1,76 +1,78 @@
 
 module.exports = class Pages {
-    constructor() {
+    constructor(views) {
         this.pages = new Map()
         this.contentTypes = new Map()
+        
+        this.fs = require('fs')
+        this.mustache = require('mustache')
+        this.marked = require('marked')
+        this.marked.setOptions({breaks: true})
+        this.TEMPLATE = this.fs.readFileSync('./static/template.mustache', 'utf8')
+        
+        if (views) {
+            for (let view of views) {
+                if(view.numOfChapters) {
+                    this.addHTMLs(view)
+                } else {
+                    this.addHTML(view)
+                }
+            }
+        }
     }
     
-    addHTML (urlPath, title, description, contentPath=false) {
-        const fs = require('fs')
-        const mustache = require('mustache')
-        const marked = require('marked')
-        marked.setOptions({breaks: true})
-        
+    addHTML (view) {
         let contentHTML
-        if (contentPath) {
-            contentHTML = fs.readFileSync(contentPath, 'utf8')
+        if (view.filePath) {
+            contentHTML = this.fs.readFileSync(view.filePath, 'utf8')
         } else {
-            const MARKDOWN = fs.readFileSync('./static' + urlPath + '.md', 'utf8')
-            contentHTML = marked(MARKDOWN)
+            const MARKDOWN = this.fs.readFileSync('./static' + view.urlPath + '.md', 'utf8')
+            contentHTML = this.marked(MARKDOWN)
         }
         
-        const TEMPLATE = fs.readFileSync('./static/template.mustache', 'utf8')
-        const HTML = mustache.render(TEMPLATE, {'description': description, 'title': title, 'body': contentHTML})
-        this.pages.set(urlPath, HTML)
+        const HTML = this.mustache.render(this.TEMPLATE, {'description': view.description, 'title': view.title, 'body': contentHTML})
+        this.pages.set(view.urlPath, HTML)
     }
-    
-    addHTMLs (urlPath, title, ...descriptions) {
-        const fs = require('fs')
-        const mustache = require('mustache')
-        const marked = require('marked')
-        marked.setOptions({breaks: true})
-        
+
+    addHTMLs (view) {
         let markdowns = []
-        for (let i in descriptions) {
-            markdowns[i] = fs.readFileSync('./static' + urlPath + '-' + (parseInt(i) + 1) + '.md', 'utf8')
+        for (let i = 1; i <= view.numOfChapters; i++) {
+            markdowns[i] = this.fs.readFileSync('./static' + view.urlPath + '-' + parseInt(i) + '.md', 'utf8')
         }
         
-        const TEMPLATE = fs.readFileSync('./static/template.mustache', 'utf8')
-        for (let i in descriptions) {
+        for (let i = 1; i <= view.numOfChapters; i++) {
             let pagination = `
                 <nav class="pagination" role="navigation" aria-label="pagination">
                     <ul class="pagination-list">`
-            for (let j in descriptions) {
+            for (let j = 1; j <= view.numOfChapters; j++) {
                 if (i === j) {
                     pagination += `
                         <li>
-                            <a class="pagination-link is-current" href="${urlPath + '-' + (parseInt(j) + 1)}">${parseInt(j) + 1}</a>
+                            <a class="pagination-link is-current" href="${view.urlPath + '-' + parseInt(j)}">${parseInt(j)}</a>
                         </li>`
                 } else {
                     pagination += `
                         <li>
-                            <a class="pagination-link" href="${urlPath + '-' + (parseInt(j) + 1)}">${parseInt(j) + 1}</a>
+                            <a class="pagination-link" href="${view.urlPath + '-' + parseInt(j)}">${parseInt(j)}</a>
                         </li>`
                 }
             }
             pagination += `</ul></nav>`
-            const HTML = mustache.render(
-                TEMPLATE,
+            const HTML = this.mustache.render(
+                this.TEMPLATE,
                 {
-                    'description': descriptions[i],
-                    'title': (title + ' ' + (parseInt(i) + 1)),
-                    'body': marked(markdowns[i]),
+                    'description': this.mustache.render(view.description, {'chapter': i}),
+                    'title': (view.title + ' ' + parseInt(i)),
+                    'body': this.marked(markdowns[i]),
                     'pagination': pagination
                 }
             )
-            this.pages.set(urlPath + '-' + (parseInt(i) + 1), HTML)
+            this.pages.set(view.urlPath + '-' + parseInt(i), HTML)
         }
-        
     }
     
     addText (urlPath) {
-        const fs = require('fs')
-        const TEXT = fs.readFileSync('.' + urlPath, 'utf8')
+        const TEXT = this.fs.readFileSync('.' + urlPath, 'utf8')
         this.pages.set(urlPath, TEXT)
         
         if (urlPath.match(/\.css$/)) {
@@ -79,8 +81,7 @@ module.exports = class Pages {
     }
     
     addBinary (urlPath) {
-        const fs = require('fs')
-        const BINARY = fs.readFileSync('.' + urlPath)
+        const BINARY = this.fs.readFileSync('.' + urlPath)
         this.pages.set(urlPath, BINARY)
         
         if (urlPath.match(/\.png$/)) {
--- server.js
+++ server.js
@@ -1,111 +1,17 @@
 
 const http = require('http')
 const Pages = require('./js/pages')
+const fs = require('fs')
 
-// Create pages
-let pages = new Pages()
-pages.addHTML(
-    '/',
-    'Home',
-    'キミの知らない世界を見せよう。',
-    './static/home.html'
-)
-pages.addHTML(
-    false,
-    'No Found',
-    '',
-    './static/no-found.html'
-)
-pages.addHTML(
-    '/about',
-    'About',
-    'このサイトについて。Fully Hatter について。'
-)
-
-// World
-pages.addHTML(
-    '/world/your-goal',
-    '「幸せ」を目指してはいけない',
-    '多くの人は「幸せ」を求めて日々生きているのではないだろうか。'
-)
-pages.addHTML(
-    '/world/happiness',
-    'あなたにとっての「幸せ」は？',
-    '「幸せ」の定義は何でしょうか？ あなたが気づいていない、「幸せ」の一つの姿について。'
-)
-pages.addHTML(
-    '/world/tips-for-your-life',
-    '今を生きるためのちょっとしたヒント',
-    '私のメモ帳の中身。気をつけないと忘れてしまうこと。でもとっても大切なこと。'
-)
-pages.addHTML(
-    '/world/freedom',
-    '「自由」に生きてはいけない',
-    'きっとキミは思うだろう。そんなはずはない！ と。でもいつかキミはこう思うだろう。確かにそうかもしれない、と。'
-)
-pages.addHTML(
-    '/world/grown-up',
-    '「大人」について',
-    'あなたは本当に「大人」ですか？'
-)
-pages.addHTML(
-    '/world/education',
-    '理想の「教育」',
-    '「歴史のテスト」を憎むすべての人に、このページを捧げます。'
-)
-pages.addHTML(
-    '/world/philosophy',
-    '「哲学」は無意味である (仮)',
-    '「この世界はどうして存在するのだろう？」Fully Hatter が答えましょう。'
-)
-pages.addHTML(
-    '/world/ideal-way-to-live-life',
-    '「一番」を目指してはいけない',
-    '「一番」を目指さずに、いったい何を目指すべきだというのか！ そんなあなたのためのページです。'
-)
-
-// Story
-pages.addHTML(
-    '/story/science-fiction/dear-you',
-    '自由なキミへ',
-    '一通の謎の手紙。拾ったのはあなた。[一話 完結]'
-)
-pages.addHTMLs(
-    '/story/science-fiction/over-the-superposition',
-    '量子の導き',
-    '第一話。そう遠くない未来、言語がひとつに統一された頃のお話。[全五話]',
-    '第二話。そう遠くない未来、言語がひとつに統一された頃のお話。[全五話]',
-    '第三話。そう遠くない未来、言語がひとつに統一された頃のお話。[全五話]',
-    '第四話。そう遠くない未来、言語がひとつに統一された頃のお話。[全五話]',
-    '第五話。そう遠くない未来、言語がひとつに統一された頃のお話。[全五話]'
-)
-pages.addHTML(
-    '/story/mystery/nightmare',
-    'ナイトメア',
-    '遊園地が舞台のちょっと怖いお話。[一話 完結]'
-)
-pages.addHTML(
-    '/story/mystery/the-dark-and-cold-place',
-    '暗くて寒い場所',
-    '部屋の明かりが消せないある女性のお話。ホラー系が苦手な人は絶対に読まないで下さい。[一話 完結]'
-)
-pages.addHTMLs(
-    '/story/comedy/baby-s-suffering',
-    '稚児の苦悩',
-    '第一話。私は赤ちゃんである。[全二話]',
-    '第二話。私は赤ちゃんである。[全二話]'
-)
-pages.addHTMLs(
-    '/story/comedy/one-man',
-    'ひとり',
-    '第一話。彗星の日に起こる不思議な出来事。[全三話]',
-    '第二話。彗星の日に起こる不思議な出来事。[全三話]',
-    '第三話。彗星の日に起こる不思議な出来事。[全三話]',
-)
+const json = fs.readFileSync('./data/views.json', 'utf8')
+let views = JSON.parse(json)
+let pages = new Pages(views)
 
 let elements = new Pages()
 elements.addText('/node_modules/bulma/css/bulma.css')
-elements.addBinary('/favicon.png')
+elements.addBinary('/icon/favicon.png')
+elements.addBinary('/icon/twitter.png')
+elements.addBinary('/icon/github.png')
 
 // Start server
 let server = http.createServer(requestListenerFunction)
