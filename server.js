710f37856bcdc53b4c8f52b2b131778380c82b40
furimako <furimako@gmail.com>
2018-02-17 08:31:27 +0900

    refactor

--- server.js
+++ server.js
@@ -7,13 +7,6 @@ const json = fs.readFileSync('./data/views.json', 'utf8')
 let views = JSON.parse(json)
 let pages = new Pages(views)
 
-let elements = new Pages()
-elements.addText('/node_modules/bulma/css/bulma.css')
-elements.addBinary('/icon/favicon.png')
-elements.addBinary('/icon/twitter.png')
-elements.addBinary('/icon/github.png')
-elements.addBinary('/icon/person.png')
-
 // Start server
 let server = http.createServer(requestListenerFunction)
 server.listen(8128)
@@ -24,14 +17,9 @@ function requestListenerFunction(request, response) {
     const urlPath = request.url
     
     if (pages.has(urlPath)) {
-        response.writeHead(200, {'Content-Type' : 'text/html'})
+        response.writeHead(200, {'Content-Type' : pages.contentType(urlPath)})
         response.end(pages.get(urlPath))
         console.log(`response page (path: ${urlPath})`)
-        
-    } else if (elements.has(urlPath)) {
-        response.writeHead(200, {'Content-Type' : elements.contentType(urlPath)})
-        response.end(elements.get(urlPath))
-        console.log(`response element (path: ${urlPath})`)
         
     } else {
         response.writeHead(404, {'Content-Type' : 'text/html'})
