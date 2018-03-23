01c20282582e0c76f599f997b7ca4dc932c04dc6
furimako <furimako@gmail.com>
2018-03-23 22:43:35 +0900

    change views and js files directories

--- js/logging.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    info: (msg) => console.log(`[${getTimestamp(new Date())}] [INFO] ${msg}`),
-    error: (msg) => console.error(`[${getTimestamp(new Date())}] [ERROR] ${msg}`)
-}
-
-let getTimestamp = (date) => {
-    return `${date.getFullYear()}/${date.getMonth()+1}/${date.getDate()} ${('00' + date.getHours()).slice(-2)}:${('00' + date.getMinutes()).slice(-2)}:${('00' + date.getSeconds()).slice(-2)}`
-}
