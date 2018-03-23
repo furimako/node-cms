d8c21ab16ebcaf89ccc756eef63f239c3784f389
furimako <furimako@gmail.com>
2018-03-23 13:32:33 +0900

    add forever installer

--- scripts/production/init/init.sh
+++ scripts/production/init/init.sh
@@ -23,3 +23,6 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF50
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.6 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list
 sudo apt-get update
 sudo apt-get install -y mongodb-org
+
+# Install forever
+sudo npm install forever -g
