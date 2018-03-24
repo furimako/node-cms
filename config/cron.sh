c34d71adeec5bfbf94dc7e690ca60d161d0c17c4
furimako <furimako@gmail.com>
2018-03-24 15:53:35 +0900

    add cron config

---
+++ config/cron.sh
@@ -0,0 +1,24 @@
+
+#!/bin/sh
+
+# 
+# Ubuntu 16.04
+# 
+
+# stop Server
+forever stop server
+
+# backup DB
+bash /home/ubuntu/fully-hatter/scripts/production/mongodump.sh
+
+# rotate log files
+DATE=$(date +%Y%m%d-%H%M%S)
+mkdir /home/ubuntu/fully-hatter/log/${DATE}
+cd /home/ubuntu/fully-hatter/log
+mv forever.log ./archives/${DATE}/forever.log
+mv stdout.log ./archives/${DATE}/stdout.log
+mv stderr.log ./archives/${DATE}/stderr.log
+
+# start Server
+cd /home/ubuntu/fully-hatter
+npm start
