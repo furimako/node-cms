30c33098c6c377059c770d7a28d0d7a8884d18e0
furimako <furimako@gmail.com>
2018-03-24 16:35:07 +0900

    update scripts for cron

--- config/cron.sh
+++ config/cron.sh
@@ -12,12 +12,12 @@ forever stop server
 bash /home/ubuntu/fully-hatter/scripts/production/mongodump.sh
 
 # rotate log files
-DATE=$(date +%Y%m%d-%H%M%S)
-mkdir /home/ubuntu/fully-hatter/log/archives/${DATE}
-cd /home/ubuntu/fully-hatter/log
-mv forever.log ./archives/${DATE}/forever.log
-mv stdout.log ./archives/${DATE}/stdout.log
-mv stderr.log ./archives/${DATE}/stderr.log
+# DATE=$(date +%Y%m%d-%H%M%S)
+# mkdir /home/ubuntu/fully-hatter/log/archives/${DATE}
+# cd /home/ubuntu/fully-hatter/log
+# mv forever.log ./archives/${DATE}/forever.log
+# mv stdout.log ./archives/${DATE}/stdout.log
+# mv stderr.log ./archives/${DATE}/stderr.log
 
 # start Server
 cd /home/ubuntu/fully-hatter
