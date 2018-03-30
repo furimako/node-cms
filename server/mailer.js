61e8bb2c331209f79db8e0363196dad47ca13571
furimako <furimako@gmail.com>
2018-03-30 14:26:00 +0900

    update mailer.js

--- server/mailer.js
+++ server/mailer.js
@@ -5,30 +5,26 @@ const pass = fs.readFileSync('./config/password.txt', 'utf8')
 
 module.exports = {
     send: (subject, text) => {
-        nodemailer.createTestAccount((err, account) => {
-            let transporter = nodemailer.createTransport({
-                host: 'smtp.gmail.com',
-                port: 465,
-                secure: true, // true for 465, false for other ports
-                auth: {
-                    user: 'furimako@gmail.com',
-                    pass
-                }
-            })
-
-            let mailOptions = {
-                from: '"Fully Hatter" <furimako@gmail.com>',
-                to: 'furimako@gmail.com',
-                subject,
-                text
+        let transporter = nodemailer.createTransport({
+            host: 'smtp.gmail.com',
+            port: 465,
+            secure: true, // true for 465, false for other ports
+            auth: {
+                user: 'furimako@gmail.com',
+                pass
             }
+        })
 
-            transporter.sendMail(mailOptions, (err, info) => {
-                if (err) {
-                    return console.log(err)
-                }
-                logging.info(`    L sent mail (${info.messageId})`)
-            })
+        const mailOptions = {
+            from: '"Fully Hatter" <furimako@gmail.com>',
+            to: 'furimako@gmail.com',
+            subject,
+            text
+        }
+
+        transporter.sendMail(mailOptions, (err, info) => {
+            if (err) { return logging.info(err) }
+            logging.info(`    L sent mail (${info.messageId})`)
         })
     }
 }
