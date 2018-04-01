9129830cdeabf54d51fca111a15094cb7426512b
furimako <furimako@gmail.com>
2018-04-01 21:31:49 +0900

    update script for git setting

--- scripts/production/init/init_git.sh
+++ scripts/production/init/init_git.sh
@@ -8,3 +8,6 @@
 sudo apt-get update
 sudo apt-get install git
 git clone https://github.com/FullyHatter/fully-hatter.git
+git config --global user.name "FullyHatter"
+git config --global user.email "furimako@gmail.com"
+git config --global push.default simple
