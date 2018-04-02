21bd42b3a96047f8e42c5dce34d1dddbc30911af
furimako <furimako@gmail.com>
2018-04-02 09:25:47 +0900

    update https setting

--- scripts/production/iptables.sh
+++ scripts/production/iptables.sh
@@ -7,3 +7,4 @@
 
 echo '['$(date +"%Y/%m/%d %H:%M:%S")'] [info] set iptables'
 sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8128
+sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 443 -j REDIRECT --to-port 8129
