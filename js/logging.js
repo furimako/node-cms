6c9e0650b4e1de6ba9bbcf2187fdbd53bd256dca
furimako <furimako@gmail.com>
2018-04-13 12:18:59 +0900

    update error logging

--- js/logging.js
+++ js/logging.js
@@ -1,7 +1,14 @@
 const dateString = require('./date_string')
+const mailer = require('./mailer')
 
 
 module.exports = {
     info: (msg) => console.log(`[${dateString.str(new Date())}] [INFO] ${msg}`),
-    error: (msg) => console.error(`[${dateString.str(new Date())}] [ERROR] ${msg}`)
+    error: (msg) => {
+        console.error(`[${dateString.str(new Date())}] [ERROR] ${msg}`)
+        mailer.send(
+            '[Fully Hatter の秘密の部屋] get ERROR',
+            `${msg}`
+        )
+    }
 }
