98f28b2c7425729adab821d93d330671d8d34019
furimako <furimako@gmail.com>
2018-09-07 00:08:08 +0900

    update mailer.js

--- js/mailer.js
+++ js/mailer.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
 let mailgun = require('mailgun-js')
-const api_key = fs.readFileSync('./config/password.txt', 'utf8')
+let api_key = fs.readFileSync('./config/password.txt', 'utf8')
+api_key = api_key.replace(/\r?\n/g, '')
 let DOMAIN = 'mg.furimako.com'
 mailgun = require('mailgun-js')({ apiKey: api_key, domain: DOMAIN })
 
