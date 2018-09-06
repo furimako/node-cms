f6fb0773de9a5d4bf43ee576003d9f80d275b495
furimako <furimako@gmail.com>
2018-09-06 19:38:48 +0900

    remove nodemailer

--- js/mailer.js
+++ js/mailer.js
@@ -1,33 +1,21 @@
 const fs = require('fs')
-const nodemailer = require('nodemailer')
-const logging = require('./logging')
-const pass = fs.readFileSync('./config/password.txt', 'utf8')
+let mailgun = require('mailgun-js')
+const api_key = fs.readFileSync('./config/password.txt', 'utf8')
+let DOMAIN = 'mg.furimako.com'
+mailgun = require('mailgun-js')({ apiKey: api_key, domain: DOMAIN })
+
 
 module.exports = {
     send: (subject, text) => {
-        let transporter = nodemailer.createTransport({
-            host: 'smtp.mailgun.org',
-            port: 465,
-            secure: true, // true for 465, false for other ports
-            auth: {
-                user: 'postmaster@mg.furimako.com',
-                pass
-            }
-        })
-
-        const mailOptions = {
+        let data = {
             from: '"Fully Hatter" <admin@furimako.com>',
             to: 'furimako@gmail.com',
             subject,
             text
         }
 
-        transporter.sendMail(mailOptions, (err, info) => {
-            if (err) {
-                logging.error(`failed to send mail\n${err}`, false)
-                return
-            }
-            logging.info(`    L sent mail (${info.messageId})`)
+        mailgun.messages().send(data, (error, body) => {
+            console.log(body)
         })
     }
 }
