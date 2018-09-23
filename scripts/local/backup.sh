1877cccc3ce1360dad4be74757a304eb48d3f71f
furimako <furimako@gmail.com>
2018-09-23 19:37:30 +0900

    update backup.sh

--- scripts/local/backup.sh
+++ scripts/local/backup.sh
@@ -6,4 +6,8 @@
 # 
 
 mkdir /Users/furimako/Dropbox/makoto/backup/fully-hatter/$(date +%Y%m%d)
+
 sudo scp -i "/Users/furimako/.ssh/LightsailDefaultPrivateKey-ap-northeast-1.pem" -r ubuntu@furimako.com:/home/ubuntu/fully-hatter/log/* /Users/furimako/Dropbox/makoto/backup/fully-hatter/$(date +%Y%m%d)/
+
+echo '['$(date +"%Y/%m/%d %H:%M:%S")'] [info] created backup'
+echo '/Users/furimako/Dropbox/makoto/backup/fully-hatter/'$(date +%Y%m%d)
