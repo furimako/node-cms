f1048076aff06ac247e3d4fdee1b9a559d12990a
furimako <furimako@gmail.com>
2018-12-12 22:00:42 +0900

    update init scripts

--- scripts/production/init/init_git.sh
+++ scripts/production/init/init_git.sh
@@ -5,8 +5,8 @@
 # Ubuntu 16.04
 # 
 
-sudo apt-get update
-sudo apt-get install git
+sudo apt update
+sudo apt upgrade
 git clone https://github.com/FullyHatter/fully-hatter.git
 git config --global user.name "FullyHatter"
 git config --global user.email "furimako@gmail.com"
