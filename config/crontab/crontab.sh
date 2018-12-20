377dbf0c352d704e850908fb6fdf334d4db4a08b
furimako <furimako@gmail.com>
2018-12-20 12:05:46 +0900

    change process manager from forever to pm2

--- config/crontab/crontab.sh
+++ config/crontab/crontab.sh
@@ -5,23 +5,21 @@
 # Ubuntu 16.04
 # 
 
-echo '['$(date +"%Y/%m/%d %H:%M:%S")'] [info] confirm crontab & forever list'
+echo '['$(date +"%Y/%m/%d %H:%M:%S")'] [info] confirm crontab & PM2 list'
 crontab -l
-forever list
+pm2 ls
 
 echo '['$(date +"%Y/%m/%d %H:%M:%S")'] [info] stop Server'
-forever stop server
+cd ~/fully-hatter
+npm stop
 
 echo '['$(date +"%Y/%m/%d %H:%M:%S")'] [info] backup mongoDB'
-bash /home/ubuntu/fully-hatter/scripts/production/mongodump.sh
+bash ~/fully-hatter/scripts/production/mongodump.sh
 
 echo '['$(date +"%Y/%m/%d %H:%M:%S")'] [info] rotate log files'
-cd /home/ubuntu/fully-hatter/log
+cd ~/fully-hatter/log
 mv server.log archives/server_`date +%Y%m%d%H%M%S`.log
 
 echo '['$(date +"%Y/%m/%d %H:%M:%S")'] [info] start Server'
-cd /home/ubuntu/fully-hatter
+cd ~/fully-hatter
 npm start
-
-echo '['$(date +"%Y/%m/%d %H:%M:%S")'] [info] confirm forever list'
-forever list
