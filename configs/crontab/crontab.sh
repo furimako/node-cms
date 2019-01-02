d2ca7d7331de76a1606b36dbf75ac19ee47b7be6
furimako <furimako@gmail.com>
2019-01-02 20:36:02 +0900

    udpate scripts

--- configs/crontab/crontab.sh
+++ configs/crontab/crontab.sh
@@ -5,21 +5,21 @@
 # Ubuntu 16.04
 # 
 
-echo "[$(date +"%Y/%m/%d %H:%M:%S")] [info] confirm crontab & PM2 list"
+echo "[$(date +'%Y/%m/%d %H:%M:%S')] [info] confirm crontab & PM2 list"
 crontab -l
 pm2 ls
 
-echo "[$(date +"%Y/%m/%d %H:%M:%S")] [info] stop Server"
+echo "[$(date +'%Y/%m/%d %H:%M:%S')] [info] stop Server"
 cd ~/fully-hatter
 npm stop
 
-echo "[$(date +"%Y/%m/%d %H:%M:%S")] [info] backup mongoDB"
+echo "[$(date +'%Y/%m/%d %H:%M:%S')] [info] backup mongoDB"
 bash ~/fully-hatter/scripts/production/mongodump.sh
 
-echo "[$(date +"%Y/%m/%d %H:%M:%S")] [info] rotate log files"
+echo "[$(date +'%Y/%m/%d %H:%M:%S')] [info] rotate log files"
 cd ~/fully-hatter/logs
-mv server.log archives/server_`date +%Y%m%d%H%M%S`.log
+mv server.log archives/server_$(date +%Y%m%d%H%M%S).log
 
-echo "[$(date +"%Y/%m/%d %H:%M:%S")] [info] start Server"
+echo "[$(date +'%Y/%m/%d %H:%M:%S')] [info] start Server"
 cd ~/fully-hatter
 npm start
