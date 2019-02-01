25a504f92102336e8acd2c22033a6be4bc43c3c1
furimako <furimako@gmail.com>
2019-02-01 15:53:21 +0900

    update logging

--- src/date_string.js
+++ src/date_string.js
@@ -1,13 +1,30 @@
-module.exports = (dateObj) => {
+module.exports = {
+    str(dateObj) {
+        const {
+            year, month, date, hours, minutes, seconds
+        } = JST(dateObj)
+        return `${year}/${month}/${date} ${hours}:${minutes}:${seconds}  JST(UTC+09:00)`
+    },
+    ISO8610(dateObj) {
+        const {
+            year, month, date, hours, minutes, seconds
+        } = JST(dateObj)
+        return `${year}-${month}-${date}T${hours}:${minutes}:${seconds}+09:00`
+    }
+}
+
+function JST(dateObj) {
     const dateJSTObj = new Date(dateObj.getTime() + 9 * 60 * 60 * 1000)
-
+    
     const year = dateJSTObj.getUTCFullYear()
-    const month = dateJSTObj.getUTCMonth() + 1
-    const date = dateJSTObj.getUTCDate()
-
+    const month = (`00${dateJSTObj.getUTCMonth() + 1}`).slice(-2)
+    const date = (`00${dateJSTObj.getUTCDate()}`).slice(-2)
+    
     const hours = (`00${dateJSTObj.getUTCHours()}`).slice(-2)
     const minutes = (`00${dateJSTObj.getUTCMinutes()}`).slice(-2)
     const seconds = (`00${dateJSTObj.getUTCSeconds()}`).slice(-2)
-
-    return `${year}/${month}/${date} ${hours}:${minutes}:${seconds}  JST(UTC+09:00)`
+    
+    return {
+        year, month, date, hours, minutes, seconds
+    }
 }
--- src/logging.js
+++ src/logging.js
@@ -1,9 +1,10 @@
+const dateString = require('./date_string')
 const mailer = require('./mailer')
 
 module.exports = {
-    info: msg => console.log(`[INFO] ${msg}`),
+    info: msg => console.log(`${dateString.ISO8610(new Date())} [INFO] ${msg}`),
     error: (msg) => {
-        console.error(`[ERROR] ${msg}`)
+        console.error(`${dateString.ISO8610(new Date())} [ERROR] ${msg}`)
         mailer.send('get ERROR', `${msg}`)
     }
 }
