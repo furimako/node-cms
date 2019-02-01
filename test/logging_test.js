57604350108ad5471dc681ca3bda7940211cbdab
furimako <furimako@gmail.com>
2019-02-01 21:54:40 +0900

    fix bug

--- test/logging_test.js
+++ test/logging_test.js
@@ -1,4 +1,4 @@
-const logging = require('../src/logging')
+const logging = require('../src/utils/logging')
 
 logging.info('execute logging_test.js (info)')
 logging.error('execute logging_test.js (error)')
