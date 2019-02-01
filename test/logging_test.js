9a0ef34a8c27c7942295f6611cdd2a07086971d1
furimako <furimako@gmail.com>
2019-02-01 11:57:21 +0900

    rename folder from "server" to "src"

--- test/logging_test.js
+++ test/logging_test.js
@@ -1,4 +1,4 @@
-const logging = require('../server/logging')
+const logging = require('../src/logging')
 
 logging.info('execute logging_test.js (info)')
 logging.error('execute logging_test.js (error)')
