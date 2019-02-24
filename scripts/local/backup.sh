e15dfa9f21168dc41a9aaa18520123c55c753608
furimako <furimako@gmail.com>
2019-02-24 17:08:27 +0900

    change the path for backup

--- scripts/local/backup.sh
+++ scripts/local/backup.sh
@@ -1,11 +1,11 @@
 
 #!/bin/sh
 
-# 
+#
 # macOS
-# 
+#
 
-local_path="/Users/furimako/Dropbox/makoto/backup/fully-hatter/$(date +%Y%m%d)"
+local_path="/Users/furimako/Documents/Dropbox/makoto/backup/fully-hatter/$(date +%Y%m%d)"
 mkdir $local_path
 
 sudo scp -i /Users/furimako/.ssh/LightsailDefaultPrivateKey-ap-northeast-1.pem -r ubuntu@furimako.com:~/fully-hatter/logs/* $local_path
