1fd2c2a8f349ad4280c0e061d5186b3b06c8f445
furimako <furimako@gmail.com>
2019-03-16 15:09:39 +0900

    change the location of server from lightsail to digitalocean

--- scripts/local/backup.sh
+++ scripts/local/backup.sh
@@ -8,6 +8,6 @@
 local_path="/Users/furimako/Documents/Dropbox/makoto/backup/fully-hatter/$(date +%Y%m%d)"
 mkdir $local_path
 
-sudo scp -i /Users/furimako/.ssh/LightsailDefaultPrivateKey-ap-northeast-1.pem -r ubuntu@furimako.com:~/fully-hatter/logs/* $local_path
+sudo scp -r furimako@furimako.com:~/fully-hatter/logs/* $local_path
 
 echo "created backup (path: ${local_path})"
