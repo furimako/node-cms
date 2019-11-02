9e7b3fa71e62aaa9c171e4b5be05e00869fad0f3
furimako <furimako@gmail.com>
2019-11-02 17:18:36 +0900

    add tags for markdown pages

---
+++ static/tags.js
@@ -0,0 +1,53 @@
+
+module.exports = {
+    幸せ: {
+        tagNameEn: 'hapiness',
+        targets: [
+            '/world/your-goal',
+            '/world/happiness',
+            '/world/death-for-life'
+        ]
+    },
+    生き方: {
+        tagNameEn: 'life',
+        targets: [
+            '/world/your-goal',
+            '/world/tips-for-your-life',
+            '/world/freedom',
+            '/world/ideal-way-to-live-life',
+            '/world/how-to-fail',
+            '/world/death-for-life'
+        ]
+    },
+    自由: {
+        tagNameEn: 'freedom',
+        targets: [
+            '/world/freedom',
+            '/world/grown-up',
+            '/world/nation'
+        ]
+    },
+    哲学: {
+        tagNameEn: 'philosophy',
+        targets: [
+            '/world/your-goal',
+            '/world/happiness',
+            '/world/philosophy',
+            '/world/death-for-life'
+        ]
+    },
+    国家: {
+        tagNameEn: 'nation',
+        targets: [
+            '/world/grown-up',
+            '/world/nation'
+        ]
+    },
+    教育: {
+        tagnameEn: 'education',
+        targets: [
+            '/world/education',
+            '/world/reward'
+        ]
+    }
+}
