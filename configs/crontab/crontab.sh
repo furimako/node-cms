7e150276956102e2f039460584f6e6a031f2853b
furimako <furimako@gmail.com>
2019-12-10 21:37:42 +0900

    renew Cearbot in cron job

--- configs/crontab/crontab.sh
+++ configs/crontab/crontab.sh
@@ -5,21 +5,24 @@
 # Ubuntu
 #
 
-echo "$(date +'%Y-%m-%dT%H:%M:%S')+09:00 [info] confirm crontab & PM2 list"
+echo "$(date +'%Y-%m-%dT%H:%M:%S')+09:00 [INFO] confirm crontab & PM2 list"
 crontab -l
 pm2 ls
 
-echo "$(date +'%Y-%m-%dT%H:%M:%S')+09:00 [info] stop Server"
+echo "$(date +'%Y-%m-%dT%H:%M:%S')+09:00 [INFO] stop Server"
 cd ~/node-cms
 pm2 stop all
 
-echo "$(date +'%Y-%m-%dT%H:%M:%S')+09:00 [info] backup MongoDB"
+echo "$(date +'%Y-%m-%dT%H:%M:%S')+09:00 [INFO] backup MongoDB"
 bash ~/node-cms/scripts/production/mongodump.sh
 
-echo "$(date +'%Y-%m-%dT%H:%M:%S')+09:00 [info] rotate log files"
+echo "$(date +'%Y-%m-%dT%H:%M:%S')+09:00 [INFO] rotate log files"
 cd ~/node-cms/logs
 mv app.log archives/app_$(date +%Y%m%d%H%M%S).log
 
-echo "$(date +'%Y-%m-%dT%H:%M:%S')+09:00 [info] start Server"
+echo "$(date +'%Y-%m-%dT%H:%M:%S')+09:00 [INFO] renew Certbot"
+sudo certbot renew
+
+echo "$(date +'%Y-%m-%dT%H:%M:%S')+09:00 [INFO] start Server"
 cd ~/node-cms
 npm start
