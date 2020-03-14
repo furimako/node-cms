82c4889b634d7e919085cd9eab12ca8f11f861d0
furimako <furimako@gmail.com>
2020-03-14 16:20:30 +0900

    update script for booting

---
+++ scripts/production/boot.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+#
+# Ubuntu
+#
+
+echo "$(date +'%Y-%m-%dT%H:%M:%S')+09:00 [INFO] confirm status"
+crontab -l
+pm2 ls
+
+echo "$(date +'%Y-%m-%dT%H:%M:%S')+09:00 [INFO] starting MongoDB"
+bash ~/node-cms/scripts/production/mongod.sh start
+
+echo "$(date +'%Y-%m-%dT%H:%M:%S')+09:00 [INFO] starting application"
+cd ~/node-cms
+npm start
