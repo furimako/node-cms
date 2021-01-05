f1a0f8ab8c0a9daf72a0ed7653eb587524829702
furimako <furimako@gmail.com>
2021-01-05 16:59:27 +0900

    update comment count script

--- scripts/count-comments.sh
+++ scripts/count-comments.sh
@@ -2,4 +2,4 @@
 
 # $1 = yyyy-mm-dd
 fromdate=$1
-mongo --eval 'db.comments.aggregate([{ $match: { date: {$gt: ISODate("'${fromdate}'T00:00:00Z")} } }, { $group: { _id: "$urlPath", count: { $sum: 1 } } }])' node-cms
+mongo --eval 'db.comments.aggregate([{ $match: { date: {$gt: ISODate("'${fromdate}'T00:00:00Z")}, lan: "ja" } }, { $group: { _id: "$urlPath", count: { $sum: 1 } } }])' node-cms
