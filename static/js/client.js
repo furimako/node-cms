d53d1a81ff29cf3a8507a577a24ae8068b96c223
furimako <furimako@gmail.com>
2021-03-12 20:01:55 +0900

    fix bugs on comment ids

--- static/js/client.js
+++ static/js/client.js
@@ -1,19 +1,3 @@
-var commentsElement = document.querySelectorAll('#comments-field > div > div.box');
-if (commentsElement) {
-    for (var i = 0; i < commentsElement.length; i += 1) {
-        commentsElement[i].setAttribute('id', `comment${i + 1}`);
-    }
-
-    var hash = window.location.hash;
-    var hashElement = false;
-    if (hash) {
-        hashElement = document.querySelector(hash);
-    }
-    if (hashElement) {
-        hashElement.scrollIntoView();
-    }
-}
-
 function openModal(id) {
     var element = document.getElementById(id);
     element.setAttribute('class', 'modal is-active');
