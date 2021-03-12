462ac47f3f1145ec519939a0059459aece3b0352
furimako <furimako@gmail.com>
2021-03-12 19:34:47 +0900

    update picked up comments

--- static/picked-up-comments.js
+++ static/picked-up-comments.js
@@ -1,9 +1,39 @@
 module.exports = {
     ja: [
         {
-            title: '「正しさ」の階層について',
+            title: 'なぜ万引きをしてはいけないのか',
+            urlPath: '/board',
+            commentNumber: 23
+        },
+        {
+            title: '「自分」中心の西洋哲学的な世界観は間違っている、というお話',
+            urlPath: '/world/sex-education',
+            commentNumber: 19
+        },
+        {
+            title: '「正しさ」について',
             urlPath: '/world/sex-education',
             commentNumber: 5
+        },
+        {
+            title: '考え続けることは思考停止である、というお話',
+            urlPath: '/world/philosophy',
+            commentNumber: 32
+        },
+        {
+            title: '「不幸」は悪いことなのか？',
+            urlPath: '/world/happiness',
+            commentNumber: 36
+        },
+        {
+            title: '統計は真理なのか？',
+            urlPath: '/world/happiness',
+            commentNumber: 32
+        },
+        {
+            title: '選択肢は多いほうがいいのか、少ないほうがいいのか',
+            urlPath: '/world/grown-up',
+            commentNumber: 17
         }
     ]
 }
