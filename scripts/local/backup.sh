400e162f4943bf7fb61e7e9cffec4c57c2080b7c
furimako <furimako@gmail.com>
2021-06-07 20:29:38 +0900

    change DigitalOcean to AWS Lightsail

--- scripts/local/backup.sh
+++ scripts/local/backup.sh
@@ -8,6 +8,6 @@
 local_path="/Users/furimako/Documents/Dropbox/makoto/5-others/backups/node-cms/$(date +%Y%m%d)"
 mkdir $local_path
 
-sudo scp -i /Users/furimako/.ssh/id_rsa -r furimako@furimako.com:~/node-cms/logs/* $local_path
+sudo scp -i ~/.ssh/LightsailDefaultKey-ap-northeast-1.pem -r ubuntu@furimako.com:~/node-cms/logs/* $local_path
 
 echo "created backup (path: ${local_path})"
