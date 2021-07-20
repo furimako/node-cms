bc43e4e37a34bea2add0f1c90207fc3aa58221ac
furimako <furimako@gmail.com>
2021-07-20 13:05:20 +0900

    update path of the script for backup

--- scripts/local/backup.sh
+++ scripts/local/backup.sh
@@ -5,7 +5,7 @@
 #
 
 echo "$(date +'%Y-%m-%dT%H:%M:%S')+09:00 [INFO] starting backup.sh"
-local_path="/Users/furimako/Documents/Dropbox/makoto/5-others/backups/node-cms/$(date +%Y%m%d)"
+local_path="/Users/furimako/Documents/Dropbox/makoto/5_others/backups/node-cms/$(date +%Y%m%d)"
 mkdir $local_path
 
 sudo scp -i ~/.ssh/LightsailDefaultKey-ap-northeast-1.pem -r ubuntu@furimako.com:~/node-cms/logs/* $local_path
