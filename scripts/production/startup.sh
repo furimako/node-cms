1cf3e4c480d6fa86ae0b895b91d2d0559c34c232
furimako <furimako@gmail.com>
2021-09-07 12:17:52 +0900

    add certbot renew in startup script

--- scripts/production/startup.sh
+++ scripts/production/startup.sh
@@ -8,6 +8,10 @@ echo "$(date +'%Y-%m-%dT%H:%M:%S')+09:00 [INFO] starting startup.sh"
 echo "$(date +'%Y-%m-%dT%H:%M:%S')+09:00 [INFO] confirm status"
 crontab -l
 
+echo "$(date +'%Y-%m-%dT%H:%M:%S')+09:00 [INFO] confirm status"
+sudo certbot renew
+sudo chmod 444 /etc/letsencrypt/live/furimako.com/privkey.pem
+
 echo "$(date +'%Y-%m-%dT%H:%M:%S')+09:00 [INFO] starting MongoDB"
 bash ~/node-cms/scripts/production/mongod.sh start
 
