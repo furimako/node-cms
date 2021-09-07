9945ae84e47cae735517206c71f584fc9829f93d
furimako <furimako@gmail.com>
2021-09-07 12:23:48 +0900

    update comment

--- scripts/production/startup.sh
+++ scripts/production/startup.sh
@@ -8,7 +8,7 @@ echo "$(date +'%Y-%m-%dT%H:%M:%S')+09:00 [INFO] starting startup.sh"
 echo "$(date +'%Y-%m-%dT%H:%M:%S')+09:00 [INFO] confirm status"
 crontab -l
 
-echo "$(date +'%Y-%m-%dT%H:%M:%S')+09:00 [INFO] confirm status"
+echo "$(date +'%Y-%m-%dT%H:%M:%S')+09:00 [INFO] renew certbot"
 sudo certbot renew
 sudo chmod 444 /etc/letsencrypt/live/furimako.com/privkey.pem
 
