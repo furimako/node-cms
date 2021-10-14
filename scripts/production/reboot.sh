89512b4475a1b6765cd78d9d876536594dd261bb
furimako <furimako@gmail.com>
2021-10-14 09:02:51 +0900

    remove sudo statement

--- scripts/production/reboot.sh
+++ scripts/production/reboot.sh
@@ -5,21 +5,13 @@
 #
 
 echo "$(date +'%Y-%m-%dT%H:%M:%S')+09:00 [INFO] starting reboot.sh"
-echo "$(date +'%Y-%m-%dT%H:%M:%S')+09:00 [INFO] update packages"
-sudo apt-get update
-sudo apt-get -y dist-upgrade
 
-echo "$(date +'%Y-%m-%dT%H:%M:%S')+09:00 [INFO] stop Server"
+echo "$(date +'%Y-%m-%dT%H:%M:%S')+09:00 [INFO]     L stop Server"
 cd ~/node-cms
 pm2 stop all
-
-echo "$(date +'%Y-%m-%dT%H:%M:%S')+09:00 [INFO] starting backup"
+echo "$(date +'%Y-%m-%dT%H:%M:%S')+09:00 [INFO]     L starting backup"
 bash ~/node-cms/scripts/production/mongodump.sh
 cd ~/node-cms/logs
 mv app.log archives/app_$(date +%Y%m%d%H%M%S).log
 
-echo "$(date +'%Y-%m-%dT%H:%M:%S')+09:00 [INFO] stop MongoDB"
-bash ~/node-cms/scripts/production/mongod.sh stop
-
-echo "$(date +'%Y-%m-%dT%H:%M:%S')+09:00 [INFO] reboot"
-sudo shutdown -r now
+echo "$(date +'%Y-%m-%dT%H:%M:%S')+09:00 [INFO] finished reboot.sh"
