cbf4a32a81d4742637026423b46fef3ac7ba2dcb
furimako <furimako@gmail.com>
2021-10-14 09:05:38 +0900

    remove sudo & remove certbot renew commands

--- scripts/production/startup.sh
+++ scripts/production/startup.sh
@@ -5,16 +5,11 @@
 #
 
 echo "$(date +'%Y-%m-%dT%H:%M:%S')+09:00 [INFO] starting startup.sh"
-echo "$(date +'%Y-%m-%dT%H:%M:%S')+09:00 [INFO] confirm status"
+
+echo "$(date +'%Y-%m-%dT%H:%M:%S')+09:00 [INFO]     L confirm crontab status"
 crontab -l
-
-echo "$(date +'%Y-%m-%dT%H:%M:%S')+09:00 [INFO] renew certbot"
-sudo certbot renew
-sudo chmod 444 /etc/letsencrypt/live/furimako.com/privkey.pem
-
-echo "$(date +'%Y-%m-%dT%H:%M:%S')+09:00 [INFO] starting MongoDB"
-bash ~/node-cms/scripts/production/mongod.sh start
-
-echo "$(date +'%Y-%m-%dT%H:%M:%S')+09:00 [INFO] starting application"
+echo "$(date +'%Y-%m-%dT%H:%M:%S')+09:00 [INFO]     L starting node-cms"
 cd ~/node-cms
 npm start
+
+echo "$(date +'%Y-%m-%dT%H:%M:%S')+09:00 [INFO] finished startup.sh"
