7b18803fd2e3d17fa793da5475389475cd267ce0
furimako <furimako@gmail.com>
2021-10-15 09:54:27 +0900

    update logging

--- scripts/production/mongodump.sh
+++ scripts/production/mongodump.sh
@@ -5,4 +5,7 @@
 #
 
 echo "$(date +'%Y-%m-%dT%H:%M:%S')+09:00 [INFO] starting mongodump.sh"
+
 mongodump --out ~/node-cms/logs/dumps/$(date +%Y%m%d-%H%M%S)/
+
+echo "$(date +'%Y-%m-%dT%H:%M:%S')+09:00 [INFO] finished mongodump.sh"
