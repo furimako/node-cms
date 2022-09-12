17fb70422af34599b08c7a221ebc292104a040dc
furimako <furimako@gmail.com>
2022-09-12 18:52:35 +0900

    update script for Linode

--- scripts/local/backup.sh
+++ scripts/local/backup.sh
@@ -8,5 +8,5 @@ echo "$(date +'%Y-%m-%dT%H:%M:%S')+09:00 [INFO] starting backup.sh"
 local_path="/Users/furimako/Documents/Dropbox/makoto/5_others/backups/node-cms/$(date +%Y%m%d)"
 mkdir $local_path
 
-sudo scp -i ~/.ssh/LightsailDefaultKey-ap-northeast-1.pem -r ubuntu@furimako.com:~/node-cms/logs/* $local_path
+sudo scp -i ~/.ssh/id_rsa -r furimako@furimako.com:~/node-cms/logs/* $local_path
 echo "created backup (path: ${local_path})"
