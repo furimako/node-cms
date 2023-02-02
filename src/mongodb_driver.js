bd03f477d107f5fc2984b90b571f6abdd2fc0d31
furimako <furimako@gmail.com>
2023-02-02 15:28:35 +0900

    update mongodb to 6.0

--- src/mongodb_driver.js
+++ src/mongodb_driver.js
@@ -26,7 +26,7 @@ module.exports = {
     async count(collectionName, filterObj) {
         const count = await this._query(
             collectionName,
-            async (collection) => collection.find(filterObj).countDocuments()
+            async (collection) => collection.countDocuments(filterObj)
         )
         logging.info(`    L found ${count} count (collection: ${collectionName}, filterObj: ${JSON.stringify(filterObj)})`)
         return count || 0
