15277004c72d0b01650aea01c385ae8ff1da8008
furimako <furimako@gmail.com>
2023-02-02 15:00:11 +0900

    update mongodb to 6.0

--- scripts/production/mongod.sh
+++ scripts/production/mongod.sh
@@ -6,5 +6,6 @@
 
 # $1 = start, stop or restart
 echo "$(date +'%Y-%m-%dT%H:%M:%S')+09:00 [INFO] executing mongod.sh"
-sudo service mongod $1
+sudo systemctl $1 mongod
+sudo systemctl status mongod
 echo 'you can see the log in [/var/log/mongodb/mongod.log]'
