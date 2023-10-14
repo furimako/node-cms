2e33b8a00007602718ca74ad54d2607085363bbc
furimako <furimako@gmail.com>
2023-10-15 08:59:59 +0900

    update path for backup

--- scripts/local/backup.sh
+++ scripts/local/backup.sh
@@ -5,7 +5,7 @@
 #
 
 echo "$(date +'%Y-%m-%dT%H:%M:%S')+09:00 [INFO] starting backup.sh"
-local_path="/Users/furimako/Documents/Dropbox/makoto/5_others/backups/node-cms/$(date +%Y%m%d)"
+local_path="/Users/furimako/Library/CloudStorage/Dropbox/makoto/5_others/backups/node-cms/$(date +%Y%m%d)"
 mkdir $local_path
 
 sudo scp -i ~/.ssh/id_rsa -r furimako@furimako.com:~/node-cms/logs/* $local_path
